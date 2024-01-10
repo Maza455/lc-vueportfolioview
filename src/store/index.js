494b742dda64d394457406a3b1d20fe4089412e1
Maza455 <same7nte@gmail.com>
2024-01-10 08:43:58 +0200

    Following what is in the Rubric

--- src/store/index.js
+++ src/store/index.js
@@ -1,117 +1,46 @@
-// import { createStore } from 'vuex'
-
-// const dataUrl = 'https://maza455.github.io/LC_portfolioData/data/'
-
-// export default createStore({
-//   state: {
-//     jobTitle: null,
-//     about: null,
-//     eduction: null,
-//     skills: null,
-//     testimonials: null,
-//     projects: null,
-//     contact: null
-//   },
-//   getters: {
-//   },
-//   mutations: {
-//     setJobTitle(state, value) {
-//       state.jobTitle = value
-//     },
-//     setAbout(state, value) {
-//       state.about = value
-//     },
-//     setEducation(state, value) {
-//       state.eduction = value
-//     },
-//     setSkills(state, value) {
-//       state.skills = value
-//     },
-//     setTestimonials(state, value) {
-//       state.testimonials = value
-//     },
-//     setProjects(state, value) {
-//       state.projects = value
-//     },
-//     setContact(state, value) {
-//       state.contact = value
-//     }
-//   },
-//   actions: {
-//     async fetchJobTitle(context) {
-//       let res = await fetch(dataUrl)
-//       let {jobTitle} = await res.json()
-//       context.commit('setJobTitle', jobTitle)
-//     },
-//     async fetchAbout(context) {
-//       // let res = await fetch(dataUrl)
-//       let {about} = await (await fetch(dataUrl)).json()
-//       context.commit('setAbout', about)
-//     }
-//   },
-//   modules: {
-//   }
-// })
-
 import { createStore } from 'vuex'
 
-const dataUrl = 'https://maza455.github.io/LC_portfolioData/data/'
+const dataUrl = 'https://maza455.github.io/LC_portfolioData/data/index.json/'
 
 export default createStore({
   state: {
-    jobTitle: null,
-    about: null,
-    eduction: null,
-    skills: null,
-    testimonials: null,
-    projects: null,
-    contact: null
-  },
-  getters: {
+    homeView: null,
+    aboutMe: null,
+    testimonials: [],
+    projects: [],
+    contact: null,
+    resume: null
   },
   mutations: {
-    setJobTitle(state, value) {
-      state.jobTitle = value
+    fetchHomeView(state, value) {
+      state.homeView = value
     },
-    setAbout(state, value) {
-      state.about = value
+    fetchAboutMe(state, value) {
+      state.aboutMe = value
     },
-    setEducation(state, value) {
-      state.education = value
-    },
-    setSkills(state, value) {
-      state.skills = value
-    },
-    setTestimonials(state, value) {
+    fetchTestimonials(state, value) {
       state.testimonials = value
     },
-    setProjects(state, value) {
+    fetchProjects(state, value) {
       state.projects = value
     },
-    setContact(state, value) {
+    fetchContact(state, value) {
       state.contact = value
+    },
+    fetchResume(state, value) {
+      state.resume = value
     }
   },
   actions: {
-    async fetchData({ commit }) {
-      try {
-        const response = await fetch(dataUrl)
-        const data = await response.json()
-
-        // Commit the data to the store
-        commit('setJobTitle', data.jobTitle)
-        commit('setAbout', data.about)
-        commit('setEducation', data.education)
-        commit('setSkills', data.skills)
-        commit('setTestimonials', data.testimonials)
-        commit('setProjects', data.projects)
-        commit('setContact', data.contact)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
+    async fetchData(context) {
+      let res = await fetch(dataUrl)
+      let data = await res.json()
+      context.commit('setHomeView', data.homeView)
+      context.commit('setAboutMe', data.aboutMe)
+      context.commit('setTestimonials', data.testimonials)
+      context.commit('setProjects', data.projects)
+      context.commit('setContact', data.reachMe)
+      context.commit('setResume', data.resume)
     }
-  },
-  modules: {
   }
 })
-
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,7 @@
       <div class="row">
         <div class="col-md-4 order-md-2">
           <div class="card border-radius-10px">
-            <img id="about-image" class="card-img-top" src="your-image-source.jpg" alt="Card image cap">
+            <img id="about-image" class="card-img-top" src="https://i.postimg.cc/jSjN25mc/419043.png" alt="Card image cap">
             <div class="card-body">
               <div class="social-links">
                 <a href="your-social-media-link"><i class="fa fa-facebook"></i></a>
@@ -20,16 +20,16 @@
             <div class="card-body">
               <h5 class="card-title">
                 <strong>
-                  <h4 id="purple-myname">MeSelf Details</h4>
+                  <h4 id="purple-name">MeSelf Details</h4>
                 </strong>
               </h5>
-              <h5>Full name:</h5> {{ aboutMe.fullname }}
-              <h5>Date of birth:</h5> {{ aboutMe.dateOfBirth }}
-              <h5>Location:</h5> {{ aboutMe.location }}
-              <h5>Cell Phone number:</h5> {{ aboutMe.cellPhoneNumber }}
-              <h5>Email Address:</h5> {{ aboutMe.emailAddress }}
+              <h5 v-if="aboutMe">Full name:</h5> <p v-if="aboutMe">{{ aboutMe.fullName }}</p>
+              <h5 v-if="aboutMe">Date of birth:</h5> <p v-if="aboutMe">{{ aboutMe.dateOfBirth }}</p>
+              <h5 v-if="aboutMe">Location:</h5> <p v-if="aboutMe">{{ aboutMe.location }}</p>
+              <h5 v-if="aboutMe">Cell Phone number:</h5> <p v-if="aboutMe">{{ aboutMe.cellPhoneNumber }}</p>
+              <h5 v-if="aboutMe">Email Address:</h5> <p v-if="aboutMe">{{ aboutMe.emailAddress }}</p>
               <p class="card-text">
-                <small id="purple-myname"><button class="btn btn-outline-success" href="your-cv-link" target="_blank">Download CV</button></small>
+                <small id="purple-name"><button class="btn btn-outline-success" href="your-cv-link" target="_blank">Download CV</button></small>
               </p>
             </div>
           </div>
@@ -39,10 +39,10 @@
             <div class="card-body">
               <h5 class="card-title">
                 <strong>
-                  <h4 id="purple-myname">Bio</h4>
+                  <h4 id="purple-name">Bio</h4>
                 </strong>
               </h5>
-              <p class="card-text">
+              <p class="card-text" v-if="aboutMe">
                 {{ aboutMe.bio }}
               </p>
             </div>
@@ -53,15 +53,14 @@
   </div>
 </template>
 
-<script>
-export default {
-  computed: {
-    aboutMe() {
-      return this.$store.state.aboutMe;
-    }
-  },
-  mounted() {
-    this.$store.dispatch('fetchAbout');
-  }
-}
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+let store = useStore()
+let aboutMe = computed(()=> store.state.about)
+
+onMounted(()=> {
+  store.dispatch('fetchAbout');
+})
 </script>
