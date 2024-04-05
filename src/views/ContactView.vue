1ee2784c74f1cbb44989f54b556da91d5c0d7d8b
Maza455 <same7nte@gmail.com>
2024-04-05 13:09:14 +0200

    Adding Updates

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,31 +9,33 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="md-form mb-0">
+                                <label for="name" class=""><b>Your name:</b></label>
                                 <input type="text" id="name" name="name" class="form-control" required>
-                                <label for="name" class="">Your name:</label>
+
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="md-form mb-0">
+                                <label for="email" class=""><b>Your email:</b></label>
                                 <input type="email" id="email" name="email" class="form-control" required>
-                                <label for="email" class="">Your email:</label>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12">
                             <div class="md-form mb-0">
+                                <label for="subject" class=""><b>Subject</b></label>
                                 <input type="text" id="subject" name="subject" class="form-control" required>
-                                <label for="subject" class="">Subject</label>
+                                
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12">
                             <div class="md-form">
+                                <label for="message"><b>Your message</b></label>
                                 <textarea type="text" id="message" name="message" rows="2"
                                     class="form-control md-textarea" required></textarea>
-                                <label for="message">Your message</label>
                             </div>
                         </div>
                     </div>
@@ -47,13 +49,13 @@
                 style="margin-top: 20px; border:solid 1px; padding-top:10px; border-radius: 50px;">
                 <ul class="list-unstyled mb-0">
                     <li>
-                        <p> CAMPUS ADDRESS</p>
+                        <p> HOME ADDRESS</p>
                         <div>
                             <iframe style="float: bottom; border-radius: 15%; border:0;"
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3308.2934215261566!2d18.490473614807716!3d-33.9849962806242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc433bfcf832a1%3A0x3b4439b8cc517802!2s314%20Imam%20Haron%20Rd%2C%20Lansdowne%2C%20Cape%20Town%2C%207780!5e0!3m2!1sen!2sza!4v1633096149800!5m2!1sen!2sza"
                                 width="200" height="120" allowfullscreen="" loading="lazy"></iframe>
                         </div>
-                        <p>314 Imam Haron Road, Cape Town, South Africa, 7780 SA</p>
+                        <p>Mitchel's plain, Cape Town, South Africa, 7785 SA</p>
                     </li>
                     <li>
                         <p>+ 27 699 950 266</p>
@@ -61,6 +63,8 @@
                     <li>
                         <i class="fas fa-envelope mt-4 fa-2x"></i>
                         <p>maza2pac77@gmail.com</p>
+                        <br>
+                        <P>eric.maza@umuzi.org</P>
                         <a style="color: rgb(7, 117, 125);" href="https://github.com/Maza455" target="_blank"><i
                                 class="bi bi-github" style="font-size: 3em;padding-bottom: 400px;"></i></a>
                         <a style="color: rgb(9, 71, 203);"
